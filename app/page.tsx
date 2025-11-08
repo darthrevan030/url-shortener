d322bfa56278a68180d401c1f05eb7538d1be68d
darth.revan.030 <smartsamarth03@gmail.com>
2025-11-09 02:52:15 +0800

    feat: implement form submission for URL shortening and improve error handling

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,34 @@ export default function Home() {
   const [shortUrl, setShortUrl] = useState("");
   const [error, setError] = useState("");
 
+  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    setError("");
+    setShortUrl("");
+
+    try{
+      const res = await fetch("/api/shorten", {
+        method: "POST",
+        headers: {
+          "Content-Type": "applications/json",
+        },
+        body: JSON.stringify({url, customText},)
+      })
+      const data = await res.json();
+
+      if (!res.ok) {
+        throw new Error(data.error || "Something went wrong!");
+      }
+      setShortUrl(data.shortUrl);
+    } catch (err) {
+    setError(err.message);
+    }
+  };
+
   return (
     <main className="p-8 max-w-2xl mx-auto">
       <h1 className="text-3xl font-bold mb-8 text-center">URL Shortener</h1>
-      <form action="" className="space-y-4">
+      <form onSubmit={handleSubmit} className="space-y-4">
         <div>
           <label htmlFor="url" className="block mb-2">
             Enter URL to shorten: 
@@ -20,7 +44,7 @@ export default function Home() {
             type="url"
             id="url"
             value={url}
-            onChange={(e) => setCustomText(e.target.value)}
+            onChange={(e) => setUrl(e.target.value)}
             required placeholder="https://example.com" 
           />
         </div>
@@ -34,7 +58,7 @@ export default function Home() {
             id="CustomText"
             value={customText}
             onChange={(e) => setCustomText(e.target.value)}
-            required placeholder="https://example.com" 
+            required placeholder="example" 
           />
         </div>
         <button type="submit" className="w-full bg-green-600 text-white py-2 rounded hover:bg-green-400 hover:text-red-400 hover:font-bold">
@@ -42,7 +66,17 @@ export default function Home() {
         </button>
       </form>
       {/* On ANY error show error box */}
-      {error && <div>{error}</div>}
+      {error && <div className="mt-4 bg-green-100 text-red-700 rounded">{error}</div>}
+
+      {/* If short URl successful show the shortened link below */}
+      {shortUrl && (
+        <div className="mt-4 p-4 bg-green-100 rounded">
+          <p>Shortened URL:</p>
+          <div className="flex items-center gap-2">
+            <input type="text" value={{shortUrl}} readOnly />
+          </div>
+        </div>
+      )}
 
     </main>
   );
